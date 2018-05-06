db02220d7290af83d35dc45614dc5a7ad6620a4d
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-06 20:31:12 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,10 +1,14 @@
 class Doctor
 
-attr_accessor :name, :appointments
+attr_accessor :name
 
 def initialize(name)
   @name = name
   @appointments = []
+end
+
+def appointments
+  @appointments
 end
 
 def add_appointment(appointment)
